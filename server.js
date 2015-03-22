233a7b1a8632a0fa5877f8ce2d35c2f05b744a8c
root <root@www.petershort.net>
2015-03-22 06:43:54 -0400

    removed bullshit logging

--- server.js
+++ server.js
@@ -7,16 +7,11 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-	console.log('user connected');
 
 	socket.on('chat message', function(msg){
-		console.log(' - ' + msg);
 		socket.emit('chat message', msg);
 	});
 
-	socket.on('disconnect', function() {
-		console.log('user disconnected');
-	});
 });
 
 http.listen(3000, function() {
